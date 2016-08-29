a1163ea092255ff143b08ebaa844abee22a1a311
Maggie Merrell <maggiemerrell31@gmail.com>
2016-08-29 11:45:07 -0400

    yay

--- src/Game.js
+++ src/Game.js
@@ -10,19 +10,19 @@ class Game extends Component {
     }
   }
 
-  onClickRenaissance(event) {
-    event.preventDefault();
-    console.log(event.target.value);
-    const gene_id = event.target.value
-    this.setState({id: gene_id})
+  componentDidMount(event) {
+    // event.preventDefault();
+    // console.log(event.target.value);
+    // const gene_id = event.target.value
+    // this.setState({id: gene_id})
     console.log('backend for game')
     const method ='post';
     const url = 'http://localhost:3000/game'
-    const dataObj = { id: gene_id}
+    // const dataObj = { id: gene_id}
     axios({
       method: method,
       url: url,
-      data: dataObj
+      // data: dat-aObj
     }).then((res) => {
       this.setState({
         response: res.data
@@ -33,10 +33,14 @@ class Game extends Component {
   }
 
   render() {
+    const returned_response = this.state.response
+    // let time_id = new Date()
     return (
       <div className="App">
         <h2 className="nav-bar">This is the game page</h2>
-        <button value="4f26f327dc7f670001000126" onClick={(event) => this.onClickRenaissance(event)}>High Renaissance</button>
+        <img src={returned_response.painting_id} />
+        {/* <button value="4f26f327dc7f670001000126" onClick={(event) => this.onClickRenaissance(event)}>High Renaissance</button> */}
+        <button>{returned_response.gene_one}</button>
       </div>
 
     );
